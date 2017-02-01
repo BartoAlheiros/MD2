be6132f390c7d3a67366779284e10c6ec6c7ba76
Bartô <alheirosb@gmail.com>
2017-01-31 23:13:21 -0300

    Testar se o conjunto é um subconjunto não vazio:
    *teste de subgrupo para conjuntos finitos:
    
    1 - testar se o conjunto digitado pelo usuário é um subconjunto não
    vazio do zn gerado.
    
    2 - testar se o conjunto digitado é fechado sob a operação de soma
    módulo n.(operação de Zn).

--- Questao_1___Andreh________/src/q2/Programa.java
+++ Questao_1___Andreh________/src/q2/Programa.java
@@ -10,25 +10,31 @@ public class Programa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		TestadorSubgrupo testador = new TestadorSubgrupo();
-		int n, conjunto[];
+		int n, conjunto[], k;
 		boolean resultado;
 		
 		System.out.println("Digite o valor de n: ");
 		n = sc.nextInt();
 		sc.nextLine(); //limpando buffer do teclado.
-		conjunto = new int[n];
+		
 		/*O número máximo de elementos que o usuário poderá digitar
 		 * é n, se não vai dar ArrayIndexOutOfBoundsException
 		 * e vai quebrar o sistema.*/
 		
-		for(int i = 0; i < n; i++) {
+		System.out.println("Digite a quantidade de elementos do candidato a subgrupo: ");
+		k = sc.nextInt();
+		sc.nextLine();
+		
+		conjunto = new int[k];
+		
+		for(int i = 0; i < k; i++) {
 			System.out.print("Digite o valor do"+ i + "o número: ");
 			conjunto[i] = sc.nextInt();
 			sc.nextLine();
 		}
 		 
 		
-		resultado = testador.teste(conjunto, n);
+		resultado = testador.testeSubgrupo(conjunto, n);
 		
 		if(resultado) {
 			System.out.println("É subconjunto.");
--- Questao_1___Andreh________/src/q2/TestadorSubgrupo.java
+++ Questao_1___Andreh________/src/q2/TestadorSubgrupo.java
@@ -4,12 +4,34 @@ package q2;
 0 <= x <= n -1 e devolva se o mesmo é um subgrupo de Zn*/
 
 public class TestadorSubgrupo {
-	public boolean teste(int[] conjunto, int n) {
+	
+	public boolean testeSubgrupo(int[] conjunto, int n) {
+		int[] z = new int[n];
+		boolean fechamento;
+		
+		z = this.gerarZn(n);
+		
+		fechamento = this.fechamento(conjunto);
+		
 		
 		
 		
 		return false;
 	}
 	
+	public int[] gerarZn(int n) {
+		int[] z = new int[n];
+		
+		for(int i = 0; i < n; i++) {
+			z[i] = i;
+		}
+		
+		return z;
+	}
+	
+	public boolean fechamento(int[] conjunto) {
+		
+		return false;
+	}
 	
 }
