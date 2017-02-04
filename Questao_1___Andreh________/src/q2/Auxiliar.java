165690272edc53e9c36196e7f3a53a718a42332d
Bartô <alheirosb@gmail.com>
2017-02-04 12:34:04 -0300

    Substituindo os métodos da questão 2 para utilizar ArrayLists:
    - Substituindo os métodos com ArrayList, em vez de Arrays, para
    facilitar;

--- Questao_1___Andreh________/src/q2/Auxiliar.java
+++ Questao_1___Andreh________/src/q2/Auxiliar.java
@@ -1,41 +1,35 @@
 package q2;
 
+import java.util.ArrayList;
+
 /*Implemente um programa que receba um conjunto de números no intervalo
 0 <= x <= n -1 e devolva se o mesmo é um subgrupo de Zn*/
 
 public class Auxiliar {
 	
-	public boolean testeSubgrupo(int[] conjunto, int n) {
-		int[] z = new int[n];
-		boolean fechamento;
-		
-		z = this.gerarZn(n);
-		
-		//fechamento = this.fechamento(conjunto);
-		
-		
-		
-		
-		return false;
-	}
-	
-	public int[] gerarZn(int n) {
-		int[] z = new int[n];
+	public ArrayList<Integer> gerarZn(int n) {
+		ArrayList<Integer> z = new ArrayList<Integer>();
 		
 		for(int i = 0; i < n; i++) {
-			z[i] = i;
+			z.add(i);
 		}
 		
 		return z;
 	}
 	
+	
+	// G eh o grupo principal e H, o candidato a subgrupo de G;
 	public boolean fechamento(int[] G, int[] H, int n) {
-		int[] soma = new int[n];
+		ArrayList<Integer> soma = new ArrayList<Integer>();
+		int j = 0;
 		
 		for(int i = 0; i < G.length; i++) {
-			for(int j = 0; j < H.length; j++) {
+			do {
+				if(G[i] == H[j] + H[j++]) {
+					
+				}
 				
-			}
+			}while(j < H.length - 2);
 		}
 		
 		
@@ -44,12 +38,12 @@ public class Auxiliar {
 	
 	
 	// recebe o candidato a subconjunto(array de inteiros e devolve um boolean que indica se ele é subconjunto ou não.
-	public boolean ehSubconjunto(int[] conjuntoOriginal, int[] candidatoASubconjunto) {
+	public boolean ehSubconjunto(ArrayList<Integer> conjuntoOriginal, ArrayList<Integer> candidatoASubconjunto) {
 		boolean resultado1 = false, resultado2 = false;
 		
-		for(int i = 0; i < candidatoASubconjunto.length; i++) {
-			for(int j = 0; j < conjuntoOriginal.length; j++){
-				if(candidatoASubconjunto[i] == conjuntoOriginal[j]) {
+		for(int i = 0; i < candidatoASubconjunto.size(); i++) {
+			for(int j = 0; j < conjuntoOriginal.size(); j++){
+				if(candidatoASubconjunto.get(i) == conjuntoOriginal.get(j)) {
 					resultado1 = true;
 				}else{
 					resultado1 = false;
--- Questao_1___Andreh________/src/q2/Programa.java
+++ Questao_1___Andreh________/src/q2/Programa.java
@@ -1,5 +1,6 @@
 package q2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /*Implemente um programa que receba um conjunto de números no intervalo
@@ -10,13 +11,15 @@ public class Programa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Auxiliar auxiliar = new Auxiliar(); //Classe auxiliar que contém métodos auxiliares para executar o algoritmo e verificar se é subgrupo ou não.
-		int n, conjunto[], k, z[];
+		ArrayList <Integer> conjunto = new ArrayList<Integer>();
+		ArrayList <Integer> z = new ArrayList<Integer>();
+		int n, k, var;
 		boolean resultado;
 		
 		System.out.println("Digite o valor de n: ");
 		n = sc.nextInt();
 		sc.nextLine(); //limpando buffer do teclado.
-		z = new int[n];
+		
 		
 		z = auxiliar.gerarZn(n);
 		
@@ -31,11 +34,10 @@ public class Programa {
 		k = sc.nextInt();
 		sc.nextLine();
 		
-		conjunto = new int[k];
 		
 		for(int i = 0; i < k; i++) {
 			System.out.print("Digite o valor do"+ i + "o número: ");
-			conjunto[i] = sc.nextInt();
+			conjunto.add(sc.nextInt());
 			sc.nextLine();
 		}
 		
